79514305c99c4e8fdd30cadddcfcebd3562cc51b
Karl Hendrik Bachmann <karla4331@gmail.com>
2023-09-28 22:58:39 +0300

    CORS fix

--- src/main/java/com/example/proovitoo/config/CorsConfig.java
+++ src/main/java/com/example/proovitoo/config/CorsConfig.java
@@ -1,2 +1,23 @@
-package com.example.proovitoo.config;public class CorsConfig {
+package com.example.proovitoo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080", "https://proovitoo.inpropartner.ee")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
