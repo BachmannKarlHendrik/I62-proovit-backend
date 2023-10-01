46e3eea8d922855537d629900a2ca33d6d272c7e
Karl Hendrik Bachmann <karla4331@gmail.com>
2023-10-01 17:17:08 +0300

    Add functionality to save bulk scores

--- src/main/java/com/example/proovitoo/controllers/ScoresRestController.java
+++ src/main/java/com/example/proovitoo/controllers/ScoresRestController.java
@@ -1,6 +1,7 @@
 package com.example.proovitoo.controllers;
 
 import com.example.proovitoo.dtos.ScorePutDto;
+import com.example.proovitoo.entities.Athlete;
 import com.example.proovitoo.entities.Score;
 import com.example.proovitoo.repositories.ScoresRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -29,5 +32,43 @@ public class ScoresRestController {
         score.getAthlete().recalculatePoints();
         scoresRepository.saveAndFlush(score);
         return ResponseEntity.ok(score);
+    }
+
+    //Kasutades eraldi PutDTO-d välistatakse võimalus, et pahatahtlikult muudetakse athleteId või eventId ära.
+    @PutMapping("scores")
+    public ResponseEntity<?> putScores(@RequestBody List<ScorePutDto> scoresList) {
+        if(scoresList.isEmpty()) {
+            return ResponseEntity.ok(scoresList);
+        }
+
+        List<Score> editedScores = new ArrayList<>();
+        Athlete athlete = null;
+
+        for (ScorePutDto scorePutDto : scoresList) {
+            Optional<Score> optional = scoresRepository.findById(scorePutDto.getId());
+            if(scorePutDto.getResult() < 0) {
+                return new ResponseEntity<>(scorePutDto + " result is smaller than 0.",HttpStatus.BAD_REQUEST);
+            }
+            if(optional.isEmpty()) {
+                return new ResponseEntity<>(scorePutDto + " no such score found.", HttpStatus.NOT_FOUND);
+            }
+            Score score = optional.get();
+            if(athlete == null) {
+                athlete = optional.get().getAthlete();
+            }
+            //Välistatakse, et kogemata on mõne muu võistleja tulemused samuti üle kirjutamisele sattunud.
+            if(!athlete.getId().equals(score.getAthlete().getId())) {
+                return new ResponseEntity<>("All scores must have the same athlete!",HttpStatus.BAD_REQUEST);
+            }
+
+            //Kui kõik testid on läbitud, lisatakse muudetud skoor muudetavate listi.
+            score.setResult(scorePutDto.getResult());
+            editedScores.add(score);
+            System.out.println(score.getEvent().getNameEst()+": "+score.getResult());
+        }
+
+        editedScores.get(0).getAthlete().recalculatePoints();
+        scoresRepository.saveAllAndFlush(editedScores);
+        return ResponseEntity.ok(editedScores);
     }
 }
